1c87f814c5f81a4aca33d3d2613d7b6c37733126
mborovac <marko.borovac.zg@gmail.com>
2016-07-24 14:04:14 +0200

    forgot some files in previous commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160711194751) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accommodations", force: :cascade do |t|
     t.string   "name",        null: false
     t.integer  "rating"
@@ -91,7 +94,7 @@ ActiveRecord::Schema.define(version: 20160711194751) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
