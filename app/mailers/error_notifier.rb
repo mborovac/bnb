891aa7d55efd3a6e01c58190a3fc096280c5dfde
mborovac <marko.borovac.zg@gmail.com>
2016-07-24 15:04:49 +0200

    error notifier cleanup

--- app/mailers/error_notifier.rb
+++ app/mailers/error_notifier.rb
@@ -9,28 +9,4 @@ class ErrorNotifier < ActionMailer::Base
     mail :to => User.where(role: [UserRole[:ADMINISTRATOR].value]).map( &:email ), :subject => "[BikeAndBed - ERROR NOTIFIER] Error on #{Rails.env}"
   end
 
-  # def transform_failed(input_document)
-  #   @input_document = input_document
-  #   mail :to => User.where(role: ["ADMINISTRATOR", "USER"]).map( &:email ), :subject => "[SAP Bridge] Error in processing Input document ##{@input_document.id}"
-  #   # mail :to => User.where.not(role: ["EXT_USER", "CASHIER", "ACCOUNTING"]).map( &:email ), :subject => "[SAP Bridge] Error in processing Input document ##{@input_document.id}"
-  # end
-  #
-  # def generator_failed(input_document, message)
-  #   @input_document = input_document
-  #   @message = message
-  #   mail :to => User.where(role: ["ADMINISTRATOR", "USER"]).map( &:email ), :subject => "[SAP Bridge] Error while generating output document for Input document ##{@input_document.id}"
-  # end
-  #
-  # def send_failed(output_document, message)
-  #   @output_document = output_document
-  #   @message = message
-  #   mail :to => User.where(role: ["ADMINISTRATOR", "USER"]).map( &:email ), :subject => "[SAP Bridge] Error while sending Output document ##{@output_document.id}"
-  # end
-  #
-  # def transaction_generator_failed(input_document, message)
-  #   @input_document = input_document
-  #   @message = message
-  #   mail :to => User.where(role: ["ADMINISTRATOR", "USER"]).map( &:email ), :subject => "[SAP Bridge] Error while generating reservation transactions based on Input document ##{@input_document.id}"
-  # end
-
 end
