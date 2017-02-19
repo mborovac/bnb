1765008e6eedd6db0ff4cbdbc6c02941d2dc0c0b
mborovac <marko.borovac.zg@gmail.com>
2017-02-19 21:01:04 +0100

    added error_mailer log info

--- app/mailers/error_notifier.rb
+++ app/mailers/error_notifier.rb
@@ -1,12 +1,14 @@
 class ErrorNotifier < ActionMailer::Base
-  default :from => YAML.load_file(PROPERTIES_PATH)['error_notifier_email_address']
+  default from: YAML.load_file(PROPERTIES_PATH)['error_notifier_email_address']
 
   def error_report(message, backtrace, user, params = nil)
     @error_details = message
     @params = params
     @user = user
     @backtrace = backtrace
-    mail :to => User.where(role: [UserRole[:ADMINISTRATOR].value]).map( &:email ), :subject => "[BikeAndBed - ERROR NOTIFIER] Error on #{Rails.env}"
+    Rails.logger.info("An error occured! @error_details: #{@error_details}, @params: #{@params}, user: #{@user}, stacktrace: #{@backtrace}")
+    mail to: User.where(role: [UserRole[:ADMINISTRATOR].value]).map(&:email),
+          subject: "[BikeAndBed - ERROR NOTIFIER] Error on #{Rails.env}"
   end
 
 end
