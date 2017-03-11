82020bf68ce1ac17bdfb7c30e2fec4ac72fc1dfe
mborovac <marko.borovac.zg@gmail.com>
2017-03-11 17:38:08 +0100

    removed unneccesary log

--- app/mailers/error_notifier.rb
+++ app/mailers/error_notifier.rb
@@ -6,7 +6,7 @@ class ErrorNotifier < ActionMailer::Base
     @params = params
     @user = user
     @backtrace = backtrace
-    Rails.logger.info("An error occured! @error_details: #{@error_details}, @params: #{@params}, user: #{@user}, stacktrace: #{@backtrace}")
+
     mail to: User.where(role: [UserRole[:ADMINISTRATOR].value]).map(&:email),
           subject: "[BikeAndBed - ERROR NOTIFIER] Error on #{Rails.env}"
   end
